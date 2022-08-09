ef46c930c58f3c04a36dbc111ccb053681717e64
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2022-08-08 22:08:37 -0700

    Add data to the spring model and view on web page

--- spring-mvc.demo/scr/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc.demo/scr/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,6 +20,28 @@ public class HelloWorldController {
 	
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	
+    //new controller method to read form data and 
+	//add data to the model
+	
+	@RequestMapping("/processFormVersion2")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		//convert data to all caps
+		theName = theName.toUpperCase();
+		
+		//create message
+		String result = "Yoooo! " + theName;
+		
+		//add message to the model
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 	
