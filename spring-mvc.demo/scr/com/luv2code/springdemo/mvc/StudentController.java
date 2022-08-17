c3096e16d119d9755cddf0d75e1ed3d408ad0c01
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2022-08-16 22:13:35 -0700

    Created student confirmation form utilizing spring mvc form tags, @modelattributs and spring mvc data binding

--- spring-mvc.demo/scr/com/luv2code/springdemo/mvc/StudentController.java
+++ spring-mvc.demo/scr/com/luv2code/springdemo/mvc/StudentController.java
@@ -2,6 +2,7 @@ package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,4 +22,25 @@ public class StudentController {
 		return "student-form";
 		
 	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student theStudent) {
+		
+		//log the input data
+		System.out.println("theStudent: " + theStudent.getFirstName()
+									+ " " + theStudent.getLastName());
+		
+		
+		return "student-confirmation";
+	}
+	
 }
+
+
+
+
+
+
+
+
+
